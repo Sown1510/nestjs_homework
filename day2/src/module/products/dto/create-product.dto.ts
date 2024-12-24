0be5cdce2f4a88907715aff8f29bd4efe0d49992
sown <sown@sowns-MacBook-Pro.local>
2024-12-24 08:33:18 +0700

    Add day2 as part of the main repository

---
+++ day2/src/module/products/dto/create-product.dto.ts
@@ -0,0 +1,30 @@
+import { IsIn, IsNotEmpty, IsNumber, IsOptional, MaxLength, Min, MinLength } from "class-validator";
+
+export class CreateProductDto {
+    @IsNotEmpty({message: 'Ten san pham khong duoc de trong'})
+    @MinLength(5, {message: 'Do dai tu 5-50 ky tu'})
+    @MaxLength(50, {message: 'Do dai tu 5-50 ky tu'})
+    name: string;
+
+    @IsOptional()
+    @MinLength(20, {message: 'Do dai tu 20-200 ky tu'})
+    @MaxLength(200, {message: 'Do dai tu 20-200 ky tu'})
+    description: string;
+
+    @IsNotEmpty({message: 'Gia san pham khong duoc de trong'})
+    @IsNumber({}, {message: 'Gia phai la mot so'})
+    @Min(1, {message: 'Gia phai la mot so lon hon 0'})
+    price: number;
+
+    @IsNotEmpty({message: 'Danh muc san pham khong duoc de trong'})
+    @IsIn(['electronics', 'furniture', 'clothing'], {message: 'Danh muc phai nam trong danh sach cho phep: electronics,furniture,clothing'})
+    category: string;
+
+    
+}
+
+// - Tên sản phẩm (name) không được để trống, độ dài từ 5-50 ký tự.
+// - Mô tả (description) không bắt buộc, nhưng nếu có phải từ 20-200 ký tự.
+// - Giá (price) phải là số dương lớn hơn 0.
+// - Danh mục (category) phải thuộc danh sách cho phép: ['electronics', 'furniture', 'clothing'].
+// - Mỗi sản phẩm có thể có danh sách các từ khóa (tags) dưới dạng mảng chuỗi, mỗi từ khóa không quá 10 ký tự.
---
+++ day2/src/module/products/products.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import * as path from 'path';
+import * as fs from 'fs';
+
+@Injectable()
+export class ProductsService {
+  private products:any[];
+  constructor(){
+    this.loadProducts();
+  }
+
+  private loadProducts() {
+    const filePath = path.resolve(process.cwd(), 'src/db/products.json');
+    const data = fs.readFileSync(filePath, 'utf-8');
+    this.products = JSON.parse(data);
+  }
+
+  private saveProducts() {
+    const filePath = path.resolve(process.cwd(), 'src/db/products.json');
+    fs.writeFileSync(filePath, JSON.stringify(this.products, null, 2), 'utf-8');
+  }
+
+  create(productData: any) {
+    this.products.push(productData);
+    this.saveProducts();
+    return this.products;
+
+  }
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    const result = this.products.find(product => product.id == id) 
+    return result
+  }
+
+  update(id: number, body: any) {
+    const newProducts = this.products.map(product => {
+      if(product.id == id) {
+        const {id, ...productUpdate} = body;
+        product = {...product, ...productUpdate}
+      }
+      return product;
+    })
+    this.products = newProducts;
+    this.saveProducts();
+    return this.products;
+  }
+
+  remove(id: number) {
+    console.log(id);
+    let isExist = false;
+    const result = this.products.filter(product => {
+      if(product.id == id)  isExist = true;
+      return product.id != id
+    })
+    if(isExist) {
+      this.products = result;
+      this.saveProducts();
+      return this.products;
+    }
+    console.log(`${id} Not exist`);
+    return this.products;
+  }
+}
